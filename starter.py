d267262898c95a930b02fbd0c5be7f619a715a13
RGuda-star <gudarish@msu.edu>
2026-02-02 16:29:59 -0500

    Adding heatmaps

--- starter.py
+++ starter.py
@@ -4,4 +4,15 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("Life Expectancy Data.csv")
 print(df.head())
 print(df.isnull().sum())
-sns.heatmap()
+df= df.drop(columns = ["Status"]) #Dropping the status column
+df = df.drop(columns = ["Country"])
+def function_heatmap(data):
+    correlation_matrix = data.corr()
+    life_expectancy_corr = correlation_matrix[['Life expectancy ']]
+    correlations_without_year = life_expectancy_corr.drop(['Year', 'Life expectancy '])
+    result = sns.heatmap(correlations_without_year)
+    return result
+
+function_heatmap(df)
+plt.tight_layout()
+plt.show()
